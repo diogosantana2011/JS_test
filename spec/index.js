249ef6db82ace477f9f9e7b387f8c5c9c7579c8e
Diogo Santana <diogo.santana@playtech.com>
2023-10-13 13:26:39 +0200

    (TASK): further jasmine tests practice

---
+++ spec/index.js
@@ -0,0 +1,45 @@
+let launchCode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: [
+        "LC101",
+        "LaunchCode Women+",
+        "CodeCamp"
+    ]
+};
+
+const launchOutput = (num) => {
+    let output = '';
+
+    if (num% 2 === 0) {
+        return output = "Launch!";
+    };
+
+    if (num% 3 === 0) {
+        return output = "Code!";
+    };
+
+    if (num% 5 === 0) {
+        return output = "Rocks!";
+    };
+
+    if (num% 2 === 0 && num% 3 === 0) {
+        return output = 'LaunchCode!';
+    };
+
+    if (num% 3 === 0 && num% 5 === 0) {
+        return output = 'Code Rocks!';
+    };
+
+    if (!output) {
+        return output = 'Rutabagas! That doesn\'t work!'
+    };
+
+    return output;
+};
+
+export { 
+    launchCode,
+    launchOutput
+}
---
+++ spec/unit/launchCode.spec.js
@@ -0,0 +1,45 @@
+// launchcode.spec.js code:
+import { launchCode, launchOutput } from '../index.js';
+
+describe('Checking launchCode fn', () => {
+    it('Checking that output of launchCode returns organization', () => {
+        expect(launchCode.organization).toEqual('nonprofit');
+    });
+
+    it('Checking that output oflaunchCode returns executiveDirector', () => {
+        expect(launchCode.executiveDirector).toEqual('Jeff');
+    });
+
+    it('Checking that output of launchCode returns percentageCoolEmployees', () => {
+        expect(launchCode.percentageCoolEmployees).toEqual(Number(100));
+    });
+
+    it('Checking that output of launchCode returns programsOffered', () => {
+        expect(launchCode.programsOffered[0]).toEqual('LC101');
+        expect(launchCode.programsOffered[1]).toEqual('LaunchCode Women+');
+        expect(launchCode.programsOffered[2]).toEqual('CodeCamp');
+        expect(launchCode.programsOffered).toHaveSize(3);
+    });
+});
+
+describe('Checks launchOutput fn', () => {
+    it('Checking that number divisible by 2 returns "Launch!"', () => {
+        expect(launchOutput(2)).toEqual('Launch!');
+    });
+
+    it('Checking that number divisible by 3 returns "Code!"', () => {
+        expect(launchOutput(21)).toEqual('Code!');
+    });
+
+    it('Checking that number divisible by 5 returns "Rocks!"', () => {
+        expect(launchOutput(5)).toEqual('Rocks!');
+    });
+
+    xit('Checking that number divisible by 2 and 3 returns "LaunchCode!"', () => {
+        expect(launchOutput(6)).toEqual('LaunchCode!');
+    });
+
+    xit('Checking that number divisible by 3 and 5 returns "Code Rocks!"', () => {
+        expect(launchOutput(15)).toEqual('Code Rocks!');
+    });    
+});
