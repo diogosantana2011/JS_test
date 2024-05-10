ff83ff8dead6e6459159b79f78373877f063befa
diogo_santana <diogo.santana@ns90.ie>
2024-05-10 11:39:16 +0100

    (TASK): Adding extra api calls to script

--- Misc/signature-api.js
+++ Misc/signature-api.js
@@ -1,9 +1,12 @@
 const crypto = require('crypto');
 
-async function generateHeaders() {
+/**
+ * Add api constants here
+ */
 
+async function generateHeaders() {
     const path = "/payment/v1/requestPaymentAddress";
-    const nonce = Date.now().toString();
+    let nonce = Date.now().toString();
     const httpMethod = "POST";
 
     const signatureContent = JSON.stringify({
@@ -33,28 +36,66 @@ async function generateHeaders() {
   
 generateHeaders();
 
-async function sendTransactionRequest() {  
+// async function sendTransactionRequest() {  
+//     const qsParams = "?address=n4piJLrXmSpqPkAQoS5bcvQZQRNkgBBYND";
+//     const path = `/accounts/v1/transaction${qsParams}`;
+//     const nonce = Date.now().toString();
+//     const httpMethod = "GET";
 
+//     const signatureContent = JSON.stringify({
+//         httpMethod,
+//         path,
+//         nonce,
+//     });
+
+//     const sig = crypto
+//         .createHmac("sha384", apiSecret)
+//         .update(signatureContent)
+//         .digest("hex");
+
+//     const headers = JSON.stringify({
+//         "x-nonce": nonce,
+//         "x-api-key": apiKey,
+//         "x-signature": sig,
+//     }, null, 3)
+//     console.log(headers)
+//     try {
+//       const res = await fetch(`https://api-staging.aquanow.io${path}`, {
+//         method: httpMethod,
+//         headers: {
+//           "x-nonce": nonce,
+//           "x-api-key": apiKey,
+//           "x-signature": sig,
+//         },
+//       });
+//       if (res.status !== 200) {
+//         throw new Error(`${(await res.json()).message} status ${res.status}`);
+//       }
+//       const result = await res.json();
+//       console.log("Result: ", result);
+//     } catch (error) {
+//       console.log(error);
+//     }
+// };
+
+// sendTransactionRequest()
+
+async function sendRequest() {  
+    const path = "/payment/v1/requestPaymentAddress";
     const nonce = Date.now().toString();
-    const httpMethod = "GET";
-
+    const httpMethod = "POST";
+  
     const signatureContent = JSON.stringify({
-        httpMethod,
-        path,
-        nonce,
+      httpMethod,
+      path,
+      nonce,
     });
-
+  
     const sig = crypto
-        .createHmac("sha384", apiSecret)
-        .update(signatureContent)
-        .digest("hex");
-
-    const headers = JSON.stringify({
-        "x-nonce": nonce,
-        "x-api-key": apiKey,
-        "x-signature": sig,
-    }, null, 3)
-    console.log(headers)
+      .createHmac("sha384", apiSecret)
+      .update(signatureContent)
+      .digest("hex");
+  
     try {
       const res = await fetch(`https://api-staging.aquanow.io${path}`, {
         method: httpMethod,
@@ -63,6 +104,12 @@ async function sendTransactionRequest() {
           "x-api-key": apiKey,
           "x-signature": sig,
         },
+        body: JSON.stringify({
+          cryptoType: "BTC",
+          fiat: "CAD",
+          fiatReceivable: 5,
+          subaccount: "3etSubaccount",
+        }),
       });
       if (res.status !== 200) {
         throw new Error(`${(await res.json()).message} status ${res.status}`);
@@ -72,6 +119,6 @@ async function sendTransactionRequest() {
     } catch (error) {
       console.log("error", error);
     }
-};
-
-sendTransactionRequest()
+}
+  
+sendRequest();
